522c04342c6ee5c7c41d562002d1e0619bf2c324
Iryna Ishchanka <iryna_ishchanka@epam.com>
2024-06-20 22:37:59 +0300

    init: start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ProjectService } from './project.service';
+import { Observable } from 'rxjs';
+
+interface Project  {
+  name: string;
+  date: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'expenses';
+export class AppComponent implements OnInit{
+  title = 'expense-tracker';
+  user!: Project | undefined;
+
+  projectFirebaseService = inject(ProjectService);
+  projectInfo$!: Observable<Project[]>;
+
+  ngOnInit(): void {
+    this.projectFirebaseService.getProjectInfo().subscribe(info => {
+      console.log(info)
+    })
+  }
 }
